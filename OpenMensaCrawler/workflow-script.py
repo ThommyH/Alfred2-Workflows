89b767c85e5a61e4f6c3c33007f9b07530f98916
Thomas Hille <thomahi@gmx.de>
2015-06-15 09:56:01 +0200

    updated to use subtitle for very long food messages

--- OpenMensaCrawler/workflow-script.py
+++ OpenMensaCrawler/workflow-script.py
@@ -10,16 +10,16 @@ DATE = ""
 BURGERMODE = False
 
 def parseInputParam():
-	input = "{query}"
-	offset = 0
-	if input == "": 
-		offset = 0
-	elif input == "morgen":
-		offset = 1
-	elif input == "burger":
-		offset = (4 - datetime.date.today().weekday())%6
-		globals()["BURGERMODE"] = True
-	globals()["DATE"] = (datetime.date.today() + datetime.timedelta(days=offset)).strftime("%Y-%m-%d")
+  input = "{query}"
+  offset = 0
+  if input == "": 
+    offset = 0
+  elif input == "morgen":
+    offset = 1
+  elif input == "burger":
+    offset = (4 - datetime.date.today().weekday())%6
+    globals()["BURGERMODE"] = True
+  globals()["DATE"] = (datetime.date.today() + datetime.timedelta(days=offset)).strftime("%Y-%m-%d")
 
 def crawlData(url):
   try:
@@ -35,6 +35,18 @@ def checkNotAvail(data, wf, image):
     return True
   return False
 
+def parseTitleSubtitle(item):
+  text_title = item["name"]
+  text_split = text_title.split(" ")
+  title = ""
+  subtitle = item['category'] + " "
+  for word in text_split:
+    if len(title+word) < 50:
+      title += word + " "
+    else:
+      subtitle += word + " "
+  return title, subtitle
+
 def getDataOpenMensa(mensaId = 0):
   url = "http://openmensa.org/api/v2/canteens/"+str(mensaId)+"/days/" + DATE + "/meals"
   return crawlData(url)
@@ -43,25 +55,28 @@ def setMensaGriebnitzsee(wf):
   data = getDataOpenMensa(62)
   if checkNotAvail(data, wf, "mensa-logo.png"): return
   for item in getDataOpenMensa(62):
-    wf.add_item(title=item['name'], subtitle=item['category'], arg="http://google.de", valid=True, icon="mensa-logo.png")
+    title, subtitle = parseTitleSubtitle(item)
+    wf.add_item(title=title, subtitle=subtitle, arg="http://google.de", valid=True, icon="mensa-logo.png")
 
 def setTastys(wf):
   data = getDataOpenMensa(104)
   if checkNotAvail(data, wf, "tastys.png"): return
   for item in data:
-	if (not globals()["BURGERMODE"] or "BURGER" in item['name']):
-	    wf.add_item(title=item['name'], subtitle=item['category'], icon="tastys.png")
+    if (not globals()["BURGERMODE"] or "BURGER" in item['name']):
+      title, subtitle = parseTitleSubtitle(item)
+      wf.add_item(title=title, subtitle=subtitle, icon="tastys.png")
 
 def setUlf(wf):
   data = getDataOpenMensa(112)
   if checkNotAvail(data, wf, "ulf.png"): return
   for item in data:
-    wf.add_item(title=item['name'], subtitle=item['category'], icon="ulf.png")
+    title, subtitle = parseTitleSubtitle(item)
+    wf.add_item(title=title, subtitle=subtitle, icon="ulf.png")
   
 def main(wf):
   if not globals()["BURGERMODE"]:
-	setMensaGriebnitzsee(wf)
-	setUlf(wf)
+    setMensaGriebnitzsee(wf)
+    setUlf(wf)
   setTastys(wf)
   wf.send_feedback()
 
